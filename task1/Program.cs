3f73a8f1aa5ea104699de59447bcbd273916dbd5
Dmitriy B <kupperfeet@gmail.com>
2022-09-01 14:53:07 +0300

    task 1 done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// заполинть двумерный массив(матрицу)
+
+void FillMatrixRandom (int[,] matrix, int min, int max)
+{
+    var random = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = random.Next(min,max+1);
+        }
+    }
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]}\t");
+
+        }
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+    }
+    
+}
+
+Console.Clear();
+int m,n;
+m = int.Parse(Console.ReadLine() ?? "0");
+n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine(" ");
+int[,] dubleArray = new int [m,n];
+FillMatrixRandom(dubleArray, -5, 10);
+PrintMatrix(dubleArray);
